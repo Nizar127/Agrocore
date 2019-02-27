a14efdbb9c34adb7f415f750a8b0a4c00fd7b7ce
Koez <ahmadfakhrul127@gmail.com>
2019-02-28 01:25:30 +0800

    first virtual supermarket at South Asia

---
+++ store/includes/footer.php
@@ -0,0 +1,96 @@
+<div id="footer"><!-- #footer Begin -->
+    <div class="container"><!-- container Begin -->
+        <div class="row"><!-- row Begin -->
+            
+            <div class="com-sm-6 col-md-3"><!-- col-sm-6 col-md-3 Begin -->
+                
+                <h4>AgroCore Sdn Bhd</h4>
+                
+                <ul><!-- ul Begin -->
+                  <li><a href="#">Home</a></li>
+                  <li><a href="#">About Us</a></li>
+				  <li><a href="#">Become our Partner</a></li>
+				  <li><a href="#">Makes Money With Us</a></li>
+				  <li><a href="#">Investor Relation</a></li>
+				  <li><a href="#">Community</a></li>
+				  <li><a href="#">Blog</a></li>
+                </ul><!-- ul Finish -->
+                
+                <hr class="hidden-md hidden-lg">
+                
+            </div><!-- col-sm-6 col-md-3 Finish -->
+            
+            <div class="col-sm-6 col-md-3"><!-- col-sm-6 col-md-3 Begin -->
+                
+                <h4>Find Us</h4>
+                
+                <p><!-- p Start -->
+                    
+                    <strong>Agrocore Sdn Bhd</strong>
+                    <br/>Fakhrul
+                    <br/>Hamizan
+					<br/>Firhad
+                    <br/>011-39608174
+                    <br/>fakhrulnizar.agrocore@gmail.com
+                    <br/><strong>Fakhrul Nizar</strong>
+                    
+                </p><!-- p Finish -->
+                
+                <a href="contact.php">Check Our Contact Page</a>
+                
+                <hr class="hidden-md hidden-lg">
+                
+            </div><!-- col-sm-6 col-md-3 Finish -->
+            
+            <div class="col-sm-6 col-md-3">
+                
+                <h4>Get The News</h4>
+                
+                <p class="text-muted">
+                    Dont miss our latest update products.
+                </p>
+                
+                <form action="https://feedburner.google.com/fb/a/mailverify" method="post" target="popupwindow" onsubmit="window.open('https://feedburner.google.com/fb/a/mailverify?uri=M-devMedia', 'popupwindow', 'scrollbars=yes,width=550,height=520');return true" method="post"><!-- form begin -->
+                    <div class="input-group"><!-- input-group begin -->
+                        
+                        <input type="text" class="form-control" name="email">
+                        
+                        <input type="hidden" value="M-devMedia" name="uri"/><input type="hidden" name="loc" value="en_US"/>
+                        
+                        <span class="input-group-btn"><!-- input-group-btn begin -->
+                            
+                            <input type="submit" value="Get The Updates" class="btn btn-danger">
+                            
+                        </span><!-- input-group-btn Finish -->
+                        
+                    </div><!-- input-group Finish -->
+                </form><!-- form Finish -->
+                
+                <hr>
+                
+                <h4>Keep In Touch</h4>
+                
+                <p class="social">
+                    <a href="#" class="fa fa-facebook"></a>
+                    <a href="#" class="fa fa-twitter"></a>
+                    <a href="#" class="fa fa-instagram"></a>
+                    <a href="#" class="fa fa-google-plus"></a>
+                    <a href="#" class="fa fa-envelope"></a>
+                </p>
+                
+            </div>
+        </div><!-- row Finish -->
+    </div><!-- container Finish -->
+</div><!-- #footer Finish -->
+
+<br>
+<div id="copyright"><!-- #copyright Begin -->
+    <div class="container"><!-- container Begin -->
+        <div class="col-md-6"><!-- col-md-6 Begin -->
+            
+            <p align="center">&copy; 2019 Agrocore Sdn Bhd</p>
+            
+        </div><!-- col-md-6 Finish -->
+
+    </div><!-- container Finish -->
+</div><!-- #copyright Finish -->
